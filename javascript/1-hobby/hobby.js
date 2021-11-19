997af7de054dad6d82193d86e45939165ca3d5ba
athoszxz <athos.kyoto@gmail.com>
2021-11-18 22:17:03 -0300

    Tarefa 1 - Javascript - Apresetação Hobby

---
+++ javascript/1-hobby/hobby.js
@@ -0,0 +1,10 @@
+document.getElementById("foto").src = "athos.png";
+document.getElementById("nome").innerHTML = "Athos Balmant";
+document.getElementById("cidade").innerHTML = "Rio de Janeiro";
+document.getElementById("idade").innerHTML = "29 Anos";
+
+document.getElementById("img-games").src = "games.jpg";
+document.getElementById("hobby").innerHTML = "Meu Hobby!";
+document.getElementById("hobby-description").innerHTML = "Gosto muito de jogar games para passar o tempo e aliviar a ansiedade. Abaixo deixo o link da minha plataforma preferida.";
+document.getElementById("link-games").href = "https://store.steampowered.com/?l=portuguese";
+document.getElementById("link-games").innerHTML = "Minha Plataforma Preferida!";
