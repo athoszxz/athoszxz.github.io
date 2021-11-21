2cb119d7a9a56a8ff96743d7052f887ae800c71a
athoszxz <athos.kyoto@gmail.com>
2021-11-21 17:15:32 -0300

    Tarefa 2 - Javascript - Cantor preferido com querySelector()

---
+++ javascript/2-cantor-querySelector/cantor.js
@@ -0,0 +1,9 @@
+const el = document.querySelector(".nome");
+const foto = document.querySelector(".cantor-foto");
+const prg = document.querySelector("#prg");
+const foto2 = document.querySelector("#cantor-foto2");
+
+el.textContent = "Michael Jackson";
+foto.src = "michael2.gif";
+prg.textContent = "Meu cantor preferido é o Michael Jackson"; 
+foto2.src = "michael3.gif";
